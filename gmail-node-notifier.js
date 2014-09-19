316e5d2d946329d5e587a5052d458ae302c03814
emiloien <emil.oien.lunde@gmail.com>
2014-09-19 19:07:37 +0000

    Added functionality to not notify the user when we have seen the unread messages before.

--- gmail-node-notifier.js
+++ gmail-node-notifier.js
@@ -51,12 +51,58 @@ function getAccessToken(oauth2Client, callback) {
 function check_for_messages() {
    gmail.users.messages.list({ userId: 'me', auth: oauth2Client, labelsIds: 'INBOX', q: 'is:unread' }, function(err, unread_messages) {
       if (!err) {
-          notifier.notify({
-            title: 'New message received',
-            message: 'You have ' + unread_messages.resultSizeEstimate + ' new message(s)'
-        }, function(error, response) {
-          process.exit();
+        console.log(unread_messages);
+        //TODO: go through the list of unread messages, find the newest message, compare its date with the one in the database
+        //if the date is newer, notify user and store date of message in database
+        //else do nothing
+        //var message_list = unread_messages.messages;
+        //check_dates(message_list, function(newest_date) {
+          //compare to database
+        //});
+        
+        var highest_unread_msg_id = unread_messages.messages[0].id;
+        db.find({ highest_msg_id: { $exists : true} }, function(err, docs) {
+          if (!err) {
+            var notify = false;
+            if (docs.length > 0) {
+              if (highest_unread_msg_id > docs[0].highest_msg_id) {
+                notify = true;
+                db.update( { highest_msg_id: { $exists : true } }, { highest_msg_id : highest_unread_msg_id });
+              }
+            }
+            else {
+              db.insert({ highest_msg_id : highest_unread_msg_id });
+              notify = true;
+            }
+            if (notify) {
+              notifier.notify({
+                title: 'New message received',
+                message: 'You have ' + unread_messages.resultSizeEstimate + ' new message(s)'
+              }, function(error, response) {
+                process.exit();
+              });
+            }
+          }
         });
+        //gmail.users.messages.get({ id: newest_msg_id, userId: 'me', auth: oauth2Client, format: 'metadata' }, get_date);
       }
   });
 }
+
+function get_date(err, msg) {
+  var date = msg.payload.headers[2].value;
+  console.log(date);
+}
+
+// function check_dates(message_list) {
+//   var newest_date = 0;
+//   for (var i = 0; i < message_list.length; i++) {
+//     gmail.users.messages.get({ id: message_list[i].id, userId: 'me', auth: oauth2Client, format: 'metadata' }, get_date);
+//   }
+//   notifier.notify({
+//     title: 'New message received',
+//     message: 'You have ' + unread_messages.resultSizeEstimate + ' new message(s)'
+//   }, function(error, response) {
+//     //process.exit();
+//   });
+// }
